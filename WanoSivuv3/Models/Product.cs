571103e5f0d0cedc4a8744ee0ac37208c2f231e8
idozemel <idozemel94@gmail.com>
2021-05-16 13:34:19 +0300

    Add project files.

---
+++ WanoSivuv3/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WanoSivuv3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Desc { get; set; }
+        public string Image { get; set; }
+        public Title Title { get; set; }
+    }
+}
