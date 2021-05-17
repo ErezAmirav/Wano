d4db822c818e68c6cbdcd66d80b6e6e2cc988319
Ionatan Schuster <yonatanmsch@cs.colman.ac.il>
2021-05-17 12:31:43 +0300

    making productsController

---
+++ WanoSivuv3/Data/WanoSivuv3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WanoSivuv3.Models;
+
+namespace WanoSivuv3.Data
+{
+    public class WanoSivuv3Context : DbContext
+    {
+        public WanoSivuv3Context (DbContextOptions<WanoSivuv3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WanoSivuv3.Models.Product> Product { get; set; }
+    }
+}
--- WanoSivuv3/Models/Product.cs
+++ WanoSivuv3/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//new include
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace WanoSivuv3.Models
 {
     public class Product
     {
+        //[Key]
         public int Id { get; set; }
+      
         public string Name { get; set; }
+        
         public double Price { get; set; }
+        
         public string Desc { get; set; }
+        
         public string Image { get; set; }
+        
         public Category Category { get; set; }
     }
 }
