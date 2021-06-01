1db5f012bdb4d99a065394d1fd9f13275259adad
YonatanSchuster <yonatanmsch@gmail.com>
2021-06-01 11:16:29 +0300

    Adding error messeges in product.cs

--- WanoSivuv3/Models/Product.cs
+++ WanoSivuv3/Models/Product.cs
@@ -10,13 +10,20 @@ namespace WanoSivuv3.Models
     {
         [Key]
         public int Id { get; set; }
-      
+
+        [Required (ErrorMessage ="Please enter a name.")]
+        [StringLength(100)]
         public string Name { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter Price.")]
         public double Price { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter a Description.")]
+        [Display(Name = "Description")]
         public string Desc { get; set; }
-        
+
+        [Required(ErrorMessage = "Please insert a Image.")]
+        [Display(Name ="Images")]
         public string Image { get; set; }
         
         public Category Category { get; set; }
