be599e7fb6f77aba4347f535978b619c8c6e945a
erezamirav <erezamirav@gmail.com>
2021-06-22 15:37:00 +0300

    added footer button to top - jquerry noder

---
+++ WanoSivuv3/wwwroot/js/backtotop.js
@@ -0,0 +1,10 @@
+﻿
+$(function () {
+    // previous detection logic
+
+    $("#to-top").on("click", function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 200);
+    });
+});
